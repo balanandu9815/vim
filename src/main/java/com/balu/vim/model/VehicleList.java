0b5b280ef7324f85ce038740317d185833e883a9
Balanandu <45590766+balanandu9815@users.noreply.github.com>
2019-01-11 12:14:44 +0530

    Add files via upload
    added improvements changes

--- src/main/java/com/balu/vim/model/VehicleList.java
+++ src/main/java/com/balu/vim/model/VehicleList.java
@@ -3,13 +3,11 @@ package com.balu.vim.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "VehicleList")
 public class VehicleList {
  
-    //@XmlElement(name = "Vehicle")
     protected List<Vehicle> vehicleList;
  
     public List<Vehicle> getVehicleList() {
